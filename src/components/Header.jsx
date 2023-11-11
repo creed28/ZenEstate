19651d321386ea514fd65422434761b1b251ed45
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-11-11 15:34:12 +0200

    Setting up the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header className='py-6 mb-12 border-b'>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to='/'>Real Estate</Link>
+        <div className='flex items-center gap-6'>
+          <Link className='hover:text-violet-900 transition' to=''>Log In</Link>
+          <Link className='hover:bg-violet-800 bg-violet-700 text-white px-4 py-3 rounded-lg transition' 
+            to=''>Sign Up</Link>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/style.css'
-import { BrowseRouter as Router} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 
 )
