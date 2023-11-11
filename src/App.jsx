0cce6051a4404427eecc8eb83d9788b030b3bd1d
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-11-11 15:21:10 +0200

    Setting up the layout(header & footer) and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { createBrowserRouter,
+  RouterProvider,
+  Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import PropertyDetails from './pages/PropertyDetails'
+
+const Layout = () => {
+  return(
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+      {
+        path:"/",
+        element: <Home />
+      },
+      {
+        path:"/property/:id",
+        element: <PropertyDetails />
+      }
+    ]
+  },
+])
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='max-w-[1440px] mx-auto bg-white'>
+     <RouterProvider router={router} />
     </div>
   )
 }
