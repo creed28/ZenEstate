6409635bd3976e6c9fbb6ffe7338bffaee5b14f3
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-11-24 17:11:50 +0200

    Extract router in a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
 import React from 'react'
 import 
-  { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } 
+  { RouterProvider } 
 from 'react-router-dom'
-import Home from './pages/Home'
-import Properties from './pages/Properties'
-import PropertyDetails from './pages/PropertyDetails'
-import About from './pages/About'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import TopAgents from './pages/TopAgents'
-import { Layout } from './layouts/Layout'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/register" element={<Register />}/>
-      <Route element={<Layout />}>
-          <Route path="/about" element={<About />}/>
-          <Route path="/properties" element={<Properties />}/>
-          <Route path="/property/:id" element={<PropertyDetails />}/>
-          <Route path="/top-agents" element={<TopAgents />}/>
-      </Route>
-    </Route>
-  )
-);
+import { router } from './routes/router'
 
 const App = () => {
   return (
-    <div className='max-w-[1440px] mx-auto bg-white'>
+    <div>
      <RouterProvider router={router} />
     </div>
   )
---
+++ src/routes/router.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom';
+import Home from '../pages/Home';
+import Properties from '../pages/Properties';
+import PropertyDetails from '../pages/PropertyDetails';
+import About from '../pages/About';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import TopAgents from '../pages/TopAgents';
+import { Layout } from '../layouts/Layout';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<Layout />}>
+        <Route path="/about" element={<About />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+        <Route path="/top-agents" element={<TopAgents />} />
+      </Route>
+    </Route>
+  )
+);
