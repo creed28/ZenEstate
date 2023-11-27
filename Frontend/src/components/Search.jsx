7da075c8caf7cd80b80195ce00358672c1b463f5
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-11-27 21:03:42 +0200

    Style Search component

--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PriceRangeDropdown from './PriceRangeDropdown';
+import PropertyDropdown from './PropertyDropdown';
+import {RiSearch2Line} from 'react-icons/ri';
 
 const Search = () => {
   return (
-    <div>
-      
+    <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-1 bg-primary-grey lg:bg-[transparent] lg:backdrop-blur rounded-lg'>
+      <PropertyDropdown />
+      <PriceRangeDropdown />
+      <button className='bg-text-color hover:bg-[#616161] transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-[white] text-lg'>
+        <RiSearch2Line />
+      </button>
     </div>
   )
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,11 +3,20 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      'primary': 'Nova Square, sans-serif',
+      primary: 'Poppins',
     },
     colors: {
       'primary-grey': '#eaeaea',
       'text-color': '#2c2d32'
+    },
+    boxShadow: {
+      1: '0px 4px 30px rgba(0, 0, 0, 0.08)',
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1234px',
     },
   },
   plugins: [],
