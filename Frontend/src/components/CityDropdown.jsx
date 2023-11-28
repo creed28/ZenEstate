1234318a7d7db777e3183d8d2b281b401c16fc07
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-11-28 11:28:12 +0200

    Design CityDropdown component + map cities options menu

--- Frontend/src/components/CityDropdown.jsx
+++ Frontend/src/components/CityDropdown.jsx
@@ -1,13 +1,45 @@
-import React, {useState, useEffect} 
+import React, {useState, useEffect, useContext} 
 from 'react';
 import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine} from 'react-icons/ri';
 import { Menu } from '@headlessui/react';
+import {HouseContext} from '../contexts/HouseContext';
 
 const CityDropdown = () => {
+  const {city, setCity, cities} = useContext(HouseContext);
+
+  console.log(cities)
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      CityDropdown
-    </div>
+    <Menu as='div' className='dropdown relative'>
+      <Menu.Button onClick={() => setIsOpen(!isOpen)} className='dropdown-btn w-full text-left'>
+        <RiMapPinLine className='dropdown-icon-primary'   />
+        <div>
+          <div className='text-[15px] font-medium leading-tight'>{city}</div>
+          <div className='text-[13px]'>Select city</div>
+        </div>
+        {
+          isOpen ? (
+            <RiArrowUpSLine className='dropdown-icon-secondary' />
+          ) : (
+            <RiArrowDownSLine className='dropdown-icon-secondary' />
+          )
+        }
+      </Menu.Button> 
+
+      <Menu.Items>
+        {cities.map((city, index) => {
+          return(
+            <Menu.Item 
+              onClick={() => setCity(city)}
+              className='cursor-pointer hover:text-[#616161] transition' as='li' key={index}>
+              {city}
+            </Menu.Item>
+          )
+        })}
+      </Menu.Items>
+    </Menu>
   )
 }
 
