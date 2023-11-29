a88906020f93823f7bb6efa86b48754a88c84f26
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-11-29 12:44:56 +0200

    Adjust styling of Search and Property components

--- Frontend/src/components/Property.jsx
+++ Frontend/src/components/Property.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {BiBed, BiBath, BiArea} from 'react-icons/bi';
 
-const Property = ({house, key}) => {
+const Property = ({house}) => {
   const {image, type, city, address, bedrooms, bathrooms, surface, price} = house;
+  
   return (
-    <div className='bg-[white] shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:drop-shadow-2xl transition'>
+    <div className='bg-primary-grey shadow-1 mt-10 p-5 rounded-lg rounded-tl-[90px]
+       w-full max-w-[352px] mx-auto cursor-pointer hover:drop-shadow-2xl transition'>
       <img className='mb-8' src={image} alt="" />
       <div className='mb-4 flex gap-x-2 text-sm'>
         <div className='rounded-full bg-[#616161] text-[white] px-3'>     
@@ -16,7 +18,7 @@ const Property = ({house, key}) => {
       </div>
       <div className='text-lg text-[#5f5f5f] font-semibold max-w-[260px]'>{address}</div>
       <div className='flex gap-x-4 my-4'>
-        <div className='flex items-center text-[#939393] gap-1'>
+        <div className='flex items-center text-[#8c8c8c] gap-1'>
           <div className='text-[20px]'>
             <BiBed />
           </div>
@@ -24,7 +26,7 @@ const Property = ({house, key}) => {
             {bedrooms}
           </div>
         </div>
-        <div className='flex items-center text-[#939393] gap-1'>
+        <div className='flex items-center text-[#8c8c8c] gap-1'>
           <div className='text-[20px]'>
             <BiBath />
           </div>
@@ -32,7 +34,7 @@ const Property = ({house, key}) => {
             {bathrooms}
           </div>
         </div>
-        <div className='flex items-center text-[#939393] gap-1'>
+        <div className='flex items-center text-[#8c8c8c] gap-1'>
           <div className='text-[20px]'>
             <BiArea />
           </div>
--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -10,13 +10,14 @@ const Search = () => {
 
   return (
     <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between 
-      gap-4 lg:gap-x-3 relative lg:top-2 lg:shadow-1 bg-primary-grey 
-      lg:bg-[#d8d8d8] lg:backdrop-blur rounded-lg'>
+      gap-4 lg:gap-x-3 relative lg:top-4 lg:shadow-1 bg-primary-grey 
+      lg:bg-[#cdcdcd] lg:backdrop-blur rounded-lg'>
       <CityDropdown />
       <PropertyDropdown />
       <PriceRangeDropdown />
-      <button onClick={() => handleClick()} className='bg-text-color hover:bg-[#616161] transition w-full lg:max-w-[162px] 
-        h-16 rounded-lg flex justify-center items-center text-[white] text-lg'>
+      <button onClick={() => handleClick()} className='bg-text-color hover:bg-[#616161] 
+        transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center
+        items-center text-[white] text-lg'>
         <RiSearch2Line />
       </button>
     </div>
