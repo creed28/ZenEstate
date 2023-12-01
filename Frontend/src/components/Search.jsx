a4d7da968862177a92c7a7d0f5d9c9c1c58ff0b0
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-12-01 18:23:29 +0200

    Make Home search button to redirect to Properties page

--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -4,9 +4,16 @@ import PriceRangeDropdown from './PriceRangeDropdown';
 import PropertyDropdown from './PropertyDropdown';
 import {RiSearch2Line} from 'react-icons/ri';
 import { HouseContext } from '../contexts/HouseContext';
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   const {handleClick} = useContext(HouseContext);
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    handleClick();
+    navigate('/properties');
+  }
 
   return (
     <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between 
@@ -15,7 +22,7 @@ const Search = () => {
       <CityDropdown />
       <PropertyDropdown />
       <PriceRangeDropdown />
-      <button onClick={() => handleClick()} className='bg-text-color hover:bg-[#616161] 
+      <button onClick={() => onClick()} className='bg-text-color hover:bg-[#616161] 
         transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center
         items-center text-[white] text-lg'>
         <RiSearch2Line />
