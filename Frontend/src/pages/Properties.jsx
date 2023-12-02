1666edca27dedd9019c04138b50f7c139e36bfe0
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-12-02 11:31:55 +0200

    Design a clear button for Property filters

--- Frontend/src/pages/Properties.jsx
+++ Frontend/src/pages/Properties.jsx
@@ -23,11 +23,18 @@ const Properties = () => {
             <CityDropdown />
             <PropertyDropdown />
             <PriceRangeDropdown />
-            <button onClick={() => handleClick()} className='bg-text-color hover:bg-[#616161] 
-                transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center
-                items-center text-[white] text-lg'>
-                <RiSearch2Line />
-            </button>
+            <div className='flex w-full justify-center gap-x-4'>
+              <button onClick={() => handleClick()} className='bg-text-color hover:bg-[#616161] 
+                  transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center
+                  items-center text-[white] text-lg'>
+                  <RiSearch2Line />
+              </button>
+              <button className='border hover:border-[#8d8d8d] hover:text-[#8d8d8d]
+                  transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center
+                  items-center text-lg'>
+                Clear
+              </button>
+            </div>
         </div>
       </div>
       <div className="w-3/4 p-4 ml-auto overflow-y-auto mt-2">
