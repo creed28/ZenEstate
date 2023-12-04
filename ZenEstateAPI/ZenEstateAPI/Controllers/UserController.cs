0599afae329af2a973f0473a149abb2c9947fcce
creed28 <hristo.zagorliev44@gmail.com>
2023-12-04 15:49:27 +0200

    Create User Controller with authentication endpoints

---
+++ ZenEstateAPI/ZenEstateAPI/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZenEstateAPI.Models.DTOs;
+using ZenEstateAPI.Services;
+using ZenEstateAPI.Services.Interfaces;
+
+namespace ZenEstateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IAuthService _authService;
+
+        public UserController(IUserService userService, IAuthService authService)
+        {
+            _userService = userService;
+            _authService = authService;
+
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _userService.RegisterUser(request);
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        public ActionResult Login(UserDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = _userService.GetUser(request);
+
+            if(user is null)
+            {
+                return BadRequest();
+            }
+
+            string token = _authService.CreateToken(user);
+
+            return Ok(new {token});
+        }
+    }  
+}
