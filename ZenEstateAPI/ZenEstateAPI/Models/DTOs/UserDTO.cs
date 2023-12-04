f09d46c69398bbed2306fc7a62af3db628645b90
creed28 <hristo.zagorliev44@gmail.com>
2023-12-04 15:50:33 +0200

    Create UserDTO

---
+++ ZenEstateAPI/ZenEstateAPI/Models/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ZenEstateAPI.Models.DTOs
+{
+    public class UserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
