de61bcb898d6bb2734ed58d1b953ca5c4290f240
creed28 <hristo.zagorliev44@gmail.com>
2023-12-04 15:49:48 +0200

    Create DB Context

---
+++ ZenEstateAPI/ZenEstateAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenEstateAPI.Models;
+
+namespace ZenEstateAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<ContractList> ContractsList { get; set; }
+    }
+}
