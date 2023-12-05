0b48e12894ba42c86b057b6d42ad5ebda5f3ff18
creed28 <hristo.zagorliev44@gmail.com>
2023-12-05 13:03:44 +0200

    Add enum for types of properties and add text length constraints

--- ZenEstateAPI/ZenEstateAPI/Models/Property.cs
+++ ZenEstateAPI/ZenEstateAPI/Models/Property.cs
@@ -1,17 +1,33 @@
-﻿namespace ZenEstateAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZenEstateAPI.Models
 {
+    public enum Type
+    {
+        House,
+        Apartment
+    }
+
     public class Property
     {
         public int Id { get; set; }
+        [StringLength(255)]
         public string Name { get; set; }
+        [StringLength(30)]
         public string City { get; set; }
+        [StringLength(255)]
         public string Address { get; set; }
-        public string Type { get; set; }
+        public Type Type { get; set; }
         public int Bedrooms { get; set; }
         public int Bathrooms { get; set; }
         public int Area { get; set; }
         public double Price { get; set; }
         public string Description {  get; set; }
-        public string ConstructionYear { get; set; }
+        public int ConstructionYear { get; set; }
+        [StringLength(255)]
+        public string ImageName { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
     }
 }
