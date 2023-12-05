5860dd3aa1c4e4a771e83ac35c2384218a25aa8f
creed28 <hristo.zagorliev44@gmail.com>
2023-12-05 11:12:30 +0200

    Wrap main with AuthContext

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/style.css'
 import HouseContextProvider from './contexts/HouseContext.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HouseContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HouseContextProvider>
+  <AuthProvider>
+    <HouseContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </HouseContextProvider>
+  </AuthProvider>
 )
