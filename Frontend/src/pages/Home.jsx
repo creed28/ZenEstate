de1924c16c95a1588b7fb233a2d6fbd18e1ff56a
creed28 <hristo.zagorliev44@gmail.com>
2023-12-05 11:13:54 +0200

    Display user's name on login

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,26 +2,35 @@ import React from 'react';
 import Image from '../assets/img/main-image.jpg';
 import Search from '../components/Search';
 import {Link} from 'react-router-dom';
+import {useAuth} from '../hooks/useAuth';
 
 const Home = () => {
+  const {auth} = useAuth();
+
   return (
     <>
         <section className='max-h-[545px] mb-8 xl:mb-24 bg-primary-grey'>
             <div className='flex flex-col lg:flex-row'>
               <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center 
               lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
-                <h1 className='uppercase text-4xl lg:text-[58px] font-semibold leading-none mb-3'>
+                <h2 className='uppercase text-4xl lg:text-[58px] font-semibold leading-none mb-3'>
                   Meet <span className='text-[#616161]'>Your </span> 
                   New Home<span className='text-[#616161]'>.</ span>
-                </h1>
+                </h2>
                 <p className='max-w-[480px] mb-5 pl-2'>
                   Finding the perfect place to call home can be both an exciting and challenging journey. 
                   Our mission is to simplify this process for you by offering a curated selection of  homes 
                   that match your unique preferences and lifestyle. Whether you're searching for a cozy apartment,
                   or a spacious house, we have a diverse range of options to explore.
                 </p>
-                <Link to={'/register'} className='bg-[#525252] text-[1.2rem] py-2 px-5 rounded-3xl 
+                {auth.user ? 
+                  <h3 className='pl-4 text-3xl lg:text-[45px] font-semibold leading-none'>
+                    Hello, <span className='text-[#616161]'>{auth.user}</span> ...
+                  </h3>
+                  : 
+                  <Link to={'/register'} className='bg-[#525252] text-[1.2rem] py-2 px-5 rounded-3xl 
                   text-[#f8f7f7] hover:bg-[#616161] ml-2'>Get Started</Link>
+                }
               </div>
              <div className='hidden flex-1 lg:flex justify-end items-end mt-10 mr-5'>
                 <img src={Image} alt="Hero House Image" className='w-[650px]' />
