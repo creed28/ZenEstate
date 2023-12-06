553b91db7df42e79683208f4f28cbc197118ce13
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 21:21:43 +0200

    Add ContractService

--- ZenEstateAPI/ZenEstateAPI/Services/ContractService.cs
+++ ZenEstateAPI/ZenEstateAPI/Services/ContractService.cs
@@ -1,5 +1,6 @@
 ﻿using ZenEstateAPI.Data;
 using ZenEstateAPI.Models;
+using ZenEstateAPI.Models.DTOs;
 using ZenEstateAPI.Services.Interfaces;
 
 namespace ZenEstateAPI.Services
@@ -13,9 +14,62 @@ namespace ZenEstateAPI.Services
             _context = context;
         }
 
+        public async Task CreateContract(PropertyDTO request, User owner)
+        {
+            string filePath = "";
+
+            if (request.ImageFile != null && request.ImageFile.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(request.ImageFile.FileName);
+
+                filePath = Path.Combine(Environment.CurrentDirectory + "/Images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await request.ImageFile.CopyToAsync(stream);
+                }
+            }
+
+            var property = new Property
+            {
+                Name = request.Name,
+                Description = request.Description,
+                City = request.City,
+                Address = request.Address,
+                Price = request.Price,
+                Bathrooms = request.Bathrooms,
+                Bedrooms = request.Bedrooms,
+                Area = request.Area,
+                ConstructionYear = request.ConstructionYear,
+                Type = request.Type,
+                ImageName = request.ImageName,
+
+            };
+            _context.Properties.Add(property);
+            await _context.SaveChangesAsync();
+            var contract = new Contract
+            {
+                Owner = owner,
+                OwnerId = request.OwnerId,
+                Property = property,
+                PropertyId = property.Id 
+            };
+            _context.Contracts.Add(contract);
+            await _context.SaveChangesAsync();
+            property.PropertyContractId = contract.Id;
+            await _context.SaveChangesAsync();
+        }
+
+        public List<Contract> GetAllContracts()
+        {
+            return _context.Contracts.ToList();
+        }
+
         public Contract? GetContract(int id)
         {
             return _context.Contracts.ToList().Find(p => p.Id == id);
         }
+
+        
     }
 }
--- ZenEstateAPI/ZenEstateAPI/Services/Interfaces/IContractService.cs
+++ ZenEstateAPI/ZenEstateAPI/Services/Interfaces/IContractService.cs
@@ -1,9 +1,12 @@
 ﻿using ZenEstateAPI.Models;
+using ZenEstateAPI.Models.DTOs;
 
 namespace ZenEstateAPI.Services.Interfaces
 {
     public interface IContractService
     {
         public Contract GetContract(int id);
+        public Task CreateContract(PropertyDTO request, User owner);
+        public List<Contract> GetAllContracts();
     }
 }
