712168fdd0db58c042bd84c774b0827638f4ec1c
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 21:21:28 +0200

    Create ContractListService

---
+++ ZenEstateAPI/ZenEstateAPI/Services/ContractListService.cs
@@ -0,0 +1,21 @@
+﻿using ZenEstateAPI.Data;
+using ZenEstateAPI.Models;
+using ZenEstateAPI.Services.Interfaces;
+
+namespace ZenEstateAPI.Services
+{
+    public class ContractListService : IContractListService
+    {
+        private readonly AppDbContext _context;
+
+        public ContractListService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ContractList? GetContractList(int id)
+        {
+            return _context.ContractsList.ToList().Find(p => p.Id == id);
+        }
+    }
+}
---
+++ ZenEstateAPI/ZenEstateAPI/Services/Interfaces/IContractListService.cs
@@ -0,0 +1,9 @@
+﻿using ZenEstateAPI.Models;
+
+namespace ZenEstateAPI.Services.Interfaces
+{
+    public interface IContractListService
+    {
+        public ContractList? GetContractList(int id);
+    }
+}
