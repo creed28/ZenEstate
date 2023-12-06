2e5fc61257b6c23cecdc11b097e05827ce001860
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 17:01:40 +0200

    Add Property Controller

---
+++ ZenEstateAPI/ZenEstateAPI/Controllers/PropertyController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZenEstateAPI.Models.DTOs;
+using ZenEstateAPI.Services;
+using ZenEstateAPI.Services.Interfaces;
+
+namespace ZenEstateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyController : ControllerBase
+    {
+        public readonly IPropertyService _propertyService;
+
+        public PropertyController(IPropertyService propertyService)
+        {
+            _propertyService = propertyService;
+        }
+
+        [HttpPost("CreateProperty")]
+        public async Task<IActionResult> CreateProperty([FromForm] PropertyDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _propertyService.CreateProperty(request);
+            return Ok();
+        }
+
+        [HttpGet("GetAllProperties")]
+        public ActionResult GetAllProperties()
+        {
+            return Ok(_propertyService.GetAllProperties());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetPropertyById(int id)
+        {
+            var property = _propertyService.GetProperty(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            var imagePath = @property.ImageName;
+
+            if (System.IO.File.Exists(imagePath))
+            {
+                // Read the image file into a byte array
+
+                var imageData = System.IO.File.ReadAllBytes(imagePath);
+                return Ok(new { property, imageData });
+                // Return the image with the appropriate content type
+                // return File(imageData, "image/jpeg");
+            }
+
+            return Ok(property);
+        }
+    }
+}
