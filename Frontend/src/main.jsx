47615bb08539b98b637725c9cac6641f6766e3f0
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 16:59:57 +0200

    Wrap App with Query Client

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,13 +4,19 @@ import App from './App.jsx'
 import './styles/style.css'
 import HouseContextProvider from './contexts/HouseContext.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <HouseContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </HouseContextProvider>
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <HouseContextProvider>
+        <React.StrictMode>
+          <App /><ReactQueryDevtools initialIsOpen={false}/>
+        </React.StrictMode>
+      </HouseContextProvider>
+    </AuthProvider>
+  </QueryClientProvider>
 )
