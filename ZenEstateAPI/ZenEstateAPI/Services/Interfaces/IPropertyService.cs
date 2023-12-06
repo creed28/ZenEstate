9eda811b5141dbe816e629f54f4bc5606b4a2e75
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 17:05:42 +0200

    Add PropertyService

---
+++ ZenEstateAPI/ZenEstateAPI/Services/Interfaces/IPropertyService.cs
@@ -0,0 +1,12 @@
+﻿using ZenEstateAPI.Models;
+using ZenEstateAPI.Models.DTOs;
+
+namespace ZenEstateAPI.Services.Interfaces
+{
+    public interface IPropertyService
+    {
+        public Task CreateProperty(PropertyDTO property);
+        public List<Property> GetAllProperties();
+        public Property GetProperty(int id);
+    }
+}
---
+++ ZenEstateAPI/ZenEstateAPI/Services/PropertyService.cs
@@ -0,0 +1,62 @@
+﻿using ZenEstateAPI.Data;
+using ZenEstateAPI.Models.DTOs;
+using ZenEstateAPI.Models;
+using ZenEstateAPI.Services.Interfaces;
+
+namespace ZenEstateAPI.Services
+{
+    public class PropertyService : IPropertyService
+    {
+        private readonly AppDbContext _context;
+
+        public PropertyService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateProperty(PropertyDTO request)
+        {
+            string filePath = "";
+
+            if (request.ImageFile != null && request.ImageFile.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(request.ImageFile.FileName);
+
+                filePath = Path.Combine(Environment.CurrentDirectory + "/Images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await request.ImageFile.CopyToAsync(stream);
+                }
+            }
+
+            var property = new Property
+            {
+                Name = request.Name,
+                Description = request.Description,
+                City = request.City,
+                Address = request.Address,
+                Price = request.Price,
+                Bathrooms = request.Bathrooms,
+                Bedrooms = request.Bedrooms,
+                Area = request.Area,
+                ConstructionYear = request.ConstructionYear,
+                Type = request.Type,
+                ImageName = filePath,
+
+            };
+            _context.Properties.Add(property);
+            await _context.SaveChangesAsync();
+        }
+
+        public List<Property> GetAllProperties()
+        {
+            return _context.Properties.ToList();
+        }
+
+        public Property? GetProperty(int id)
+        {
+            return  _context.Properties.ToList().Find(p => p.Id == id);
+        }
+    }
+}
