f35a73738395fbae7caeb469610a7ec29813df62
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 17:03:54 +0200

    Add ContractId

--- ZenEstateAPI/ZenEstateAPI/Models/ContractList.cs
+++ ZenEstateAPI/ZenEstateAPI/Models/ContractList.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public Contract Contract { get; set; }
+        public int ContractId { get; set; }
         public int UserId { get; set; }
         public User Customer { get; set; }
     }
