447593fdc165e18fa0af48d297eb50b056002c78
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 19:05:08 +0200

    Style Create Property page

--- Frontend/src/pages/CreateProperty.jsx
+++ Frontend/src/pages/CreateProperty.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import LoginImage from '../assets/img/login-image.avif';
+import CreatePropertyImage from '../assets/img/create-page-photo.jpg';
 import Logo from '../assets/icons/logo.png';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
 
 const CreateProperty = () => {
@@ -9,15 +9,17 @@ const CreateProperty = () => {
   const [description, setDescription] = useState('');
   const [city, setCity] = useState('');
   const [address, setAddress] = useState('');
-  const [bathrooms, setBathrooms] = useState(0);
-  const [bedrooms, setBedrooms] = useState(0);
-  const [area, setArea] = useState(0);
-  const [price, setPrice] = useState(0);
-  const [year, setYear] = useState(0);
+  const [bathrooms, setBathrooms] = useState('');
+  const [bedrooms, setBedrooms] = useState('');
+  const [area, setArea] = useState('');
+  const [price, setPrice] = useState('');
+  const [year, setYear] = useState('');
   const [type, setType] = useState('');
   const [image, setImage] = useState(null);
   const [imageName, setImageName] = useState('');
+  const [errors, setErrors] = useState('');
   const navigate = useNavigate();
+
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -27,6 +29,37 @@ const CreateProperty = () => {
 
   const handleSubmit = async () => {
     try {
+      
+      if (isNaN(area) || area <= 0) {
+        setErrors('Area must be a positive number');
+        return;
+      }
+  
+      if (isNaN(price) || price <= 0) {
+        setErrors('Price must be a positive number');
+        return;
+      }
+  
+      if (isNaN(year) || year <= 0) {
+        setErrors('Year must be a positive number');
+        return;
+      }
+  
+      if (isNaN(bathrooms) || bathrooms <= 0) {
+        setErrors('Bathrooms must be a positive number');
+        return;
+      }
+  
+      if (isNaN(bedrooms) || bedrooms <= 0) {
+        setErrors('Bedrooms must be a positive number');
+        return;
+      }
+
+      if (!name || !city || !address || !price || !year || !bathrooms || !area || !bedrooms || !type || !image) {
+        setErrors('*All fields are required');
+        return;
+      }
+
       const data = new FormData();
       data.append('name', name);
       data.append('description', description);
@@ -52,11 +85,10 @@ const CreateProperty = () => {
     <main className='w-full h-screen flex items-start'>
       <section className='w-1/2 h-full flex flex-col'>
         <div className='absolute top-[14%] left-[5%] flex flex-col'>
-          <h1 className='text-4xl text-[white] text-center font-bold my-4'>Find Your Dream Home Today</h1>
-          <p className='text-xl text-[white] font-normal'>Start for free and browse through the best offers 
-          from +90k customers.</p>
+          <h1 className='text-4xl text-[white] text-center font-bold my-4'>Help Others Find Their Home</h1>
+          <p className='text-xl text-[white] font-normal'>List a Property of Your Own And Join Our Community.</p>
         </div>
-        <img src={LoginImage} className='w-full h-full object-cover' alt="" />
+        <img src={CreatePropertyImage} className='w-full h-full object-cover' alt="" />
       </section>
 
       <section className='w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20 justify-between items-center'>
@@ -64,73 +96,60 @@ const CreateProperty = () => {
           <img src={Logo} alt="" className='w-[35px] h-[35px]'  />
          <h2 className='text-3xl font-semibold'>ZenEstate</h2>
         </div>
-
+        <p className='text-[red] text-sm mt-1'>{errors}</p>
         <div className='w-full flex flex-col justify-center max-w-[550px]'>
-          <div className='flex flex-col w-full mb-2'>
-            <h3 className='text-3xl font-semibold mb-2'>Login</h3>
-            <p className='text-base mb-2'>Welcome Back! Please enter your details.</p>
-          </div>
-
           <form className='w-full flex flex-col'>
             <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
              outline-none focus:outline-none' 
-            placeholder='Email'
+            placeholder='Name'
             type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
-             outline-none focus:outline-none' 
-            placeholder='Password'
-            type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
              <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
              outline-none focus:outline-none' 
-            placeholder='Email'
+            placeholder='City'
             type="text" value={city} onChange={(e) => setCity(e.target.value)} />
              <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
              outline-none focus:outline-none' 
-            placeholder='Email'
-            type="email" value={address} onChange={(e) => setAddress(e.target.value)} />
+            placeholder='Address'
+            type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
              <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
              outline-none focus:outline-none' 
-            placeholder='Email'
-            type="number" value={area} onChange={(e) => setArea(e.target.value)} />
-             <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
-             outline-none focus:outline-none' 
-            placeholder='Email'
+            placeholder='Price'
             type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
              <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
              outline-none focus:outline-none' 
-            placeholder='Email'
+            placeholder='Year'
             type="number" value={year} onChange={(e) => setYear(e.target.value)} />
              <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
              outline-none focus:outline-none' 
-            placeholder='Email'
-            type="file" onChange={handleImageChange} />
-             <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
-             outline-none focus:outline-none' 
-            placeholder='Email'
+            placeholder='Bathrooms'
             type="number" value={bathrooms} onChange={(e) => setBathrooms(e.target.value)} />
+            <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
+             outline-none focus:outline-none' 
+            placeholder='Area'
+            type="number" value={area} onChange={(e) => setArea(e.target.value)} />
              <input className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
              outline-none focus:outline-none' 
-            placeholder='Email'
+            placeholder='Bedrooms'
             type="number" value={bedrooms} onChange={(e) => setBedrooms(e.target.value)} />
-            <div>
-              <input checked={type === 'house'} type="radio" name='type' value='house' id='house' 
-              onChange={e => setType(e.target.value)} />
-              <label htmlFor="house">House</label>
-              <input checked={type === 'apartment'} type="radio" name='type' value='apartment' id='apartment' 
-              onChange={e => setType(e.target.value)} />
-              <label htmlFor="apartment">Apartment</label>
+            <div className='flex gap-x-4'>
+              <div className='flex justify-center items-center gap-x-2'>
+                <input checked={type === 'house'} type="radio" name='type' value='house' id='house' 
+                onChange={e => setType(e.target.value)} className='pt-3' />
+                <label htmlFor="house">House</label>
+              </div>
+              <div className='flex justify-center items-center gap-x-2'>
+                <input checked={type === 'apartment'} type="radio" name='type' value='apartment' id='apartment' 
+                onChange={e => setType(e.target.value)} className='pt-3' />
+                <label htmlFor="apartment">Apartment</label>
+              </div>
             </div>
+            <textarea className='w-full text-[black] py-2 my-2 bg-[transparent] border-b border-[black]
+             outline-none focus:outline-none resize-none overflow-y-scroll' 
+            placeholder='Description'
+            type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+            <input className='w-full text-[black] py-2 my-2 bg-[transparent]
+             outline-none focus:outline-none' type="file" onChange={handleImageChange} />
           </form>
-
-          <div className='w-full flex items-center justify-between'>
-            <div className='w-full flex items-center'>
-              <input type="checkbox" className='w-4 h-4 mr-2' />
-              <p className='text-sm'>Remember Me</p>
-            </div>
-
-            <p className='text-sm cursor-pointer underline underline-offset-2 font-medium whitespace-nowrap'>
-              Forgot Password?</p>
-          </div>
 
           <div className='w-full flex flex-col my-4'>
             <button onClick={() => handleSubmit()} className='w-full bg-text-color text-[white] 
