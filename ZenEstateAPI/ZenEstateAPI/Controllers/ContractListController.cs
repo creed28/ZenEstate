96043b6b620c2a128c1a9ca697d33f3ced8605c5
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 21:18:47 +0200

    Create ContractListController

---
+++ ZenEstateAPI/ZenEstateAPI/Controllers/ContractListController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZenEstateAPI.Services.Interfaces;
+
+namespace ZenEstateAPI.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ContractListController : ControllerBase
+        {
+
+            public readonly IContractListService _contractListService;
+
+            public ContractListController(IContractListService contractListService)
+            {
+                _contractListService = contractListService;
+            }
+
+            [HttpGet("{id}")]
+            public ActionResult GetContractById(int id)
+            {
+                var contractList = _contractListService.GetContractList(id);
+                if (contractList == null)
+                {
+                    return NotFound();
+                }
+                return Ok(contractList);
+            }
+        }
+}
