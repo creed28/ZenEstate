02f54a48229f0b0a616e20cb9032146e0d9afcba
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 17:04:08 +0200

    Create PropertyDTO

---
+++ ZenEstateAPI/ZenEstateAPI/Models/DTOs/PropertyDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZenEstateAPI.Models.DTOs
+{
+    public class PropertyDTO
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public Type Type { get; set; }
+        public int Area { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int ConstructionYear { get; set; }
+        public string ImageName { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
