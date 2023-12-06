6f7513a4af20034e9d743d94aa0ce212ef3c8586
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 21:17:59 +0200

    Add data queries

--- Frontend/src/pages/PropertyDetails.jsx
+++ Frontend/src/pages/PropertyDetails.jsx
@@ -1,14 +1,62 @@
 import React from 'react';
-import {housesData} from '../../../API/data';
 import {useParams} from 'react-router-dom';
 import {BiBed, BiBath, BiArea} from 'react-icons/bi';
 import {Link} from 'react-router-dom';
+import axios from '../api/axios';
+import { useQuery } from '@tanstack/react-query';
 
 const PropertyDetails = () => {
   const {id} = useParams();
-  const house = housesData.find(house => {
-    return house.id === parseInt(id);
-  })
+  const {data: contract} = useQuery({ 
+    queryKey: ["contract", id],
+    queryFn: () => getContract(id),
+  });
+
+  const getContract = async (id) => {
+    try {
+      const res = await axios.get(`/Contract/${id}`, id);
+      console.log(res.data)
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const propertyId = contract?.propertyId;
+  const ownerId = contract?.ownerId;
+
+  const {data: property} = useQuery({ 
+    queryKey: ["property", propertyId],
+    queryFn: () => getProperty(propertyId),
+    enabled:!!propertyId
+  });
+
+  const getProperty = async (id) => {
+    try {
+      const res = await axios.get(`/Property/${id}`, id);
+      // const imageUrl = URL.createObjectURL(new Blob([res.data.imageData]));
+      // setNewImage(imageUrl); 
+      console.log(res.data);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const {data: user} = useQuery({ 
+    queryKey: ["user", ownerId],
+    queryFn: () => getUser(ownerId),
+    enabled:!!ownerId
+  });
+
+  const getUser = async (id) => {
+    try {
+      const res = await axios.get(`/User/${id}`, id);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <section>
@@ -16,56 +64,56 @@ const PropertyDetails = () => {
         <div className='flex flex-col items-start gap-8 lg:flex-row'>
           <div className='max-w-[768px] mt-4'>
             <div className='mb-4'>
-              <img src={house.imageLg} alt="House Image" />
+              <img src={property?.imageName} alt="House Image" />
             </div>
             <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between'>
               <div>
                 <h2 className='text-2xl font-semibold'>
-                  {house.name}
+                  {property?.name}
                 </h2>
                 <h3 className='text-lg mb-4'>
-                  {house.address}
+                  {property?.address}
                 </h3>
               </div>
               <div className='mb-4 lg:mb-0 flex gap-x-2 text-sm'>
                 <div className='rounded-full bg-[#616161] text-[white] px-3'>
-                  {house.type}
+                  {property?.type}
                 </div>
                 <div className='bg-[#888888] rounded-full text-[white] px-3'>
-                  {house.city}
+                  {property?.city}
                 </div>
               </div>
               <div className='text-3xl font-semibold'>
-                {house.price} BGN
+                {property?.price} BGN
               </div>
             </div>
             <div className='flex gap-x-6 mb-6'>
               <div className='flex gap-x-2 items-center'>
                 <BiBed className='text-2xl' />
-                <div>{house.bedrooms}</div>
+                <div>{property?.bedrooms}</div>
               </div>
               <div className='flex gap-x-2 items-center'>
                 <BiBath className='text-2xl' />
-                <div>{house.bathrooms}</div>
+                <div>{property?.bathrooms}</div>
               </div>
               <div className='flex gap-x-2 items-center'>
                 <BiArea className='text-2xl' />
-                <div>{house.surface}</div>
+                <div>{property?.surface}</div>
               </div>
             </div>
-            <div>{house.description}</div>
+            <div>{property?.description}</div>
           </div>
           <div className='flex-1 w-full mb-8 border border-text-color rounded-lg px-6 py-8
            mt-4 bg-primary-grey'>
             <div className='flex items-center gap-x-4 mb-8'>
               <div className='w-20 h-20 p-1 border border-text-color rounded-full'>
-                <img src={house.agent.image} alt="Agent Image" />
+                <img src={user?.image} alt="Agent Image" />
               </div>
               <div>
                 <div className='font-bold text-lg'>
-                  {house.agent.name}
+                  {user?.name}
                 </div>
-                <p className='text-[14px]'>{house.agent.phone}</p>
+                <p className='text-[14px]'>{user?.phone}</p>
                 <Link to='' className='text-sm'>View Listings</Link>
               </div>
             </div>
