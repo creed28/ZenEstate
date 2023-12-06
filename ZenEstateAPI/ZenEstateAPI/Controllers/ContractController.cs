f80a24c8120cb94694a6b384a6d06b23f521b7ea
creed28 <hristo.zagorliev44@gmail.com>
2023-12-06 17:01:25 +0200

    Add Contract Controller

---
+++ ZenEstateAPI/ZenEstateAPI/Controllers/ContractController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZenEstateAPI.Services;
+using ZenEstateAPI.Services.Interfaces;
+
+namespace ZenEstateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractController : ControllerBase
+    {
+
+        public readonly IContractService _contractService;
+
+        public ContractController(IContractService contractService)
+        {
+            _contractService = contractService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetContractById(int id)
+        {
+            var contract = _contractService.GetContract(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+            return Ok(contract);
+        }
+    }
+}
